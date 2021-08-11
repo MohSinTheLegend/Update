1c5a5bd2a34c4a4e0d8c77d069f39a28f8cb0959
Ꮇ0ᎻᏚᏆN ᎪᏞᏆ <72184388+MohSinTheLegend@users.noreply.github.com>
2021-08-11 07:39:00 +0500

    Create .m

---
+++ .m
@@ -0,0 +1,41 @@
+import os, re platform
+
+
+
+def Mohsin():
+    #os.system('clear')
+    #os.system('termux-setup-storage')
+    global user
+    try:
+        r = open('Mohsin.txt','r').read()
+    except IOError:
+        name = "None"
+        user = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:40.0) Gecko/20100101 Firefox/40.0'
+        m=open('mohsin.txt','w')
+        m.write(user)
+        m.close()
+        os.system ("rm -rf matrix.txt ")
+        os.system ("pip2 install ipaddress")
+        os.system ('pip2 install lolcat')
+        os.system ('pip2 install requests')
+        bit=platform.architecture()[0]
+        if bit=="64bit":
+            import apcb
+            apcb.gf()
+        elif bit=="32bit":
+            import apcs
+            apcs.gf()
+    os.system ("rm -rf matrix.txt")
+    os.system ("pip2 install ipaddress")
+    os.system ('pip2 install lolcat')
+    os.system ('pip2 install requests')
+    bit=platform.architecture()[0]
+    if bit=="64bit":
+        import apcb
+        apcb.gf()
+    elif bit=="32bit":
+        import apcs
+        apcs.gf()
+    
+if __name__ == '__main__':
+    Mohsin()
